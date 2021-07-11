42fb0297dadaf83e37651aa23b1978e1abfb8f4f
Jumana <modijumana5@gmail.com>
2021-07-11 16:55:16 +0530

    Added css

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 let a;
 let date;
 let time;
-const options1 = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-const options2 = {hour: '2-digit', minute: '2-digit' , second: '2-digit'};
+let day;
+let seconds;
+const options1 = {year: 'numeric', month: 'long', day: 'numeric' };
+const options2 = {hour: '2-digit', minute: '2-digit' , hour12: false};
+const options4 = {second: '2-digit' , hour12: false};
+const option3 = {weekday: 'long'};
 setInterval(() => {
     a = new Date();
-    // time = a.getHours() + ":" + a.getMinutes() + ":" + a.getSeconds();
+
     time = a.toLocaleTimeString("en-us", options2);
     date = a.toLocaleDateString(undefined, options1);
-    document.getElementById('time').innerHTML = "Current Time is: " + time + "<br>On " + date + "<br><br>";
+    day=a.toLocaleDateString(undefined,option3);
+    seconds=a.toLocaleTimeString(undefined,options4);
+
+    document.getElementById('time').innerHTML = time;
+    document.getElementById('day').innerHTML = day;
+    document.getElementById('date').innerHTML = date;
+    document.getElementById('seconds').innerHTML = ":" + seconds;
+    
 }, 1000);
